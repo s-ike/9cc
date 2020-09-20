7775c32bfbcfdee3dd4427ee373ac3b1bb419f11
s-ike <shunt_i@yahoo.co.jp>
2020-09-20 17:15:41 +0900

    ファイル分割

---
+++ 9cc.h
@@ -0,0 +1,81 @@
+#ifndef __9CC_H__
+#define __9CC_H__
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+// tokenize.c
+//
+
+// トークンの種類
+typedef enum
+{
+	TK_RESERVED,	// 記号
+	TK_NUM,			// 整数トークン
+	TK_EOF,			// 入力の終わりを表すトークン
+} TokenKind;
+
+// トークン型
+typedef struct Token Token;
+struct Token
+{
+	TokenKind	kind;	// トークンの型
+	Token		*next;	// 次の入力トークン
+	int			val;	// KindがTK_NUMの場合、その数値
+	char		*str;	// トークン文字列
+	int			len;	// トークンの長さ
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+long expect_number();
+bool at_eof();
+Token *tokenize(void);
+
+extern char		*user_input;
+extern Token	*token;
+
+//
+// parse.c
+//
+
+// 抽象構文木のノードの種類
+typedef enum
+{
+	ND_ADD,	// +
+	ND_SUB,	// -
+	ND_MUL,	// *
+	ND_DIV,	// /
+	ND_EQ,	// ==
+	ND_NE,	// !=
+	ND_LT,	// <
+	ND_LE,	// <=
+	ND_NUM,	// 数値
+} NodeKind;
+
+// 抽象構文木（AST）のノードの型
+typedef struct Node Node;
+struct Node
+{
+	NodeKind	kind;	// ノードの型
+	Node		*lhs;	// 左辺
+	Node		*rhs;	// 右辺
+	long		val;	// kindがND_NUMの場合のみ使用
+};
+
+Node *expr(void);
+
+//
+// Code generator
+//
+
+void codegen(Node *node);
+
+#endif
---
+++ parse.c
@@ -0,0 +1,120 @@
+#include "9cc.h"
+
+static Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+	Node	*node;
+
+	node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+static Node *new_node_num(int val)
+{
+	Node	*node;
+
+	node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+static Node *equality(void);
+static Node *relational(void);
+static Node *add(void);
+static Node *mul(void);
+static Node *unary(void);
+static Node *primary(void);
+
+Node *expr(void)
+{
+	return equality();
+}
+
+static Node *equality(void)
+{
+	Node	*node = relational();
+
+	while (1)
+	{
+		if (consume("=="))
+			node = new_node(ND_EQ, node, relational());
+		else if (consume("!="))
+			node = new_node(ND_NE, node, relational());
+		else
+			return node;
+	}
+}
+
+static Node *relational(void)
+{
+	Node	*node = add();
+
+	while (1)
+	{
+		if (consume("<"))
+			node = new_node(ND_LT, node, add());
+		else if (consume("<="))
+			node = new_node(ND_LE, node, add());
+		else if (consume(">"))
+			node = new_node(ND_LT, add(), node);
+		else if (consume(">="))
+			node = new_node(ND_LE, add(), node);
+		else
+			return node;
+	}
+}
+
+static Node *add(void)
+{
+	Node	*node = mul();
+
+	while (1)
+	{
+		if (consume("+"))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume("-"))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+static Node *mul(void)
+{
+	Node	*node = unary();
+
+	while (1)
+	{
+		if (consume("*"))
+			node = new_node(ND_MUL, node, unary());
+		else if (consume("/"))
+			node = new_node(ND_DIV, node, unary());
+		else
+			return node;
+	}
+}
+
+static Node *unary(void)
+{
+	if (consume("+"))
+		return unary();
+	else if (consume("-"))
+		return new_node(ND_SUB, new_node_num(0), unary());
+	else
+		return primary();
+}
+
+static Node *primary(void)
+{
+	if (consume("("))
+	{
+		Node *node = expr();
+		expect(")");
+		return node;
+	}
+	// 数値
+	return new_node_num(expect_number());
+}
